ab63760a2e36f5e838463b380767091de15091b8
Dmytro Cisar <i@dmyt.ro>
2017-11-07 12:34:55 +0200

    add flex-grow pattern

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: 'pattern',
+    id: 'flex-grow-emmet',
+    name: 'Flex Grow Emmet',
+    matcher: 'Fxg',
+    allowParamToValue: true,
+    styles: {
+      'flex-grow': '$0',
+    }
+  }
+];
